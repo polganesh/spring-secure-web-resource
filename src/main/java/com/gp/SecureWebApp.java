de3c81748cdb75c18c9d5e9734194d0248262dd9
Ganesh Pol <polganesh@gmail.com>
2017-08-20 16:53:55 +0530

    using github as resource server

--- src/main/java/com/gp/SecureWebApp.java
+++ src/main/java/com/gp/SecureWebApp.java
@@ -5,12 +5,14 @@ package com.gp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author GANESH
  *
  */
 @SpringBootApplication
+@EnableResourceServer
 public class SecureWebApp {
 
 	/**
